98aa86933a618c4b7b8ad95a6b35f84f5c3b5aa1
Victorien HUIBANT <huibant.victorien@gmail.com>
2017-12-12 16:09:15 +0100

    maj exo7

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
-<?php
-$name = 'HUIBANT';
-$lastname = 'Victorien';
-$age = 25;
-echo ('Bonjour '.$name.' '.$lastname.', tu as '.$age.' ans.')
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>exercice 7 partie 1 php</title>
+  </head>
+  <body>
+    <?php
+    $name = 'HUIBANT';
+    $lastname = 'Victorien';
+    $age = 25;
+    echo ('Bonjour '.$name.' '.$lastname.', tu as '.$age.' ans.')
+    ?>
+  </body>
+</html>
